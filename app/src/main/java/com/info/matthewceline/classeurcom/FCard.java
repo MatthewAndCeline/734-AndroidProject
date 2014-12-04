b24ecc2ebaa25320b7c39fe1e3307fa449a72268
Céline de Roland <celinederoland@itpassion.info>
2014-12-04 08:47:54 +0100

    Squelette des classes Card du pattern Composite

---
+++ app/src/main/java/com/info/matthewceline/classeurcom/FCard.java
@@ -0,0 +1,18 @@
+package com.info.matthewceline.classeurcom;
+
+/**
+ * Created by celine on 04/12/14.
+ * Final Element (pattern Composite)
+ */
+public class FCard extends Card {
+
+    FCard(int _id, String _title, String _picture) {
+        super(_id, _title, _picture);
+    }
+
+    @Override
+    String getType() {
+        return "F";
+    }
+
+}
