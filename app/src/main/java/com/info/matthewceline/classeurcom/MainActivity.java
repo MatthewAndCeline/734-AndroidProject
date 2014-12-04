73d8c1f49c325db9df61f06d9a4eb2bb9ce2d931
Céline de Roland <celinederoland@itpassion.info>
2014-12-04 08:39:51 +0100

    squelette de la classe MainActivity

--- app/src/main/java/com/info/matthewceline/classeurcom/MainActivity.java
+++ app/src/main/java/com/info/matthewceline/classeurcom/MainActivity.java
@@ -1,9 +1,12 @@
 package com.info.matthewceline.classeurcom;
 
+import android.app.Dialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,4 +17,111 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //                                       Listeners                                           //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    private class ClicCard implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class LongClicCard implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class ClicAddBt implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class ClicAdmin implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class ClicHelp implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class ClicBack implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class ClicUndo implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //                                       Dialogues                                           //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        //id available values :
+            //0 -> 19999 = Card Id
+            //20000 = admin
+            //21000 = help
+            //22000 = add a Card
+
+        Dialog box = new Dialog(this);
+        box.setTitle("Un dialogue");
+
+        return box;
+    }
+
+    @Override
+    protected void onPrepareDialog(int id, @NonNull Dialog box) {
+
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //                           Manage User Interface                                           //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void updateUI() {
+
+    }
+
+    private void displaySentence() {
+
+    }
+
+    private void displayPage() {
+
+    }
+
+    private void displayAdminDialog() {
+
+    }
+
+    private void displayAddDialog() {
+
+    }
 }
